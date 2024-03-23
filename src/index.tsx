59200ffc64ad3c18dee8ec17373c1cc1815e1429
nagamoridaiki <44520472+nagamoridaiki@users.noreply.github.com>
2024-03-23 13:29:42 +0900

    商品詳細/カート内/出品登録ページのHTMLとCSS作成 (#2)

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import MainPage from './MainPage'
+import DetailPage from './DetailPage'
+import CartPage from './CartPage'
+import ProductRegistration from './ProductRegistration'
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
@@ -10,7 +13,8 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <MainPage />
+    {/* <MainPage /> */}
+    <ProductRegistration />
   </React.StrictMode>
 );
 

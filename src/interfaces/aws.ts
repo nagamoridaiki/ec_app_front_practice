be8d279acbe5bdebb0e949d37db882ca8091b6f0
nagamoridaiki <no.swea1017@gmail.com>
2024-05-30 00:36:37 +0900

    .envのクレデンシャル情報を修正

--- src/interfaces/aws.ts
+++ src/interfaces/aws.ts
@@ -1,10 +1,10 @@
 import AWS from 'aws-sdk';
 
-export const S3_BUCKET = process.env.S3_BUCKET_NAME;
-export const REGION = process.env.REGION;
+export const S3_BUCKET = process.env.NEXT_PUBLIC_S3_BUCKET_NAME;
+export const REGION = process.env.NEXT_PUBLIC_REGION;
 
 AWS.config.update({
-  accessKeyId: process.env.ACCESS_KEY_ID,
-  secretAccessKey: process.env.SECRET_ACCESS_KEY,
+  accessKeyId: process.env.NEXT_PUBLIC_ACCESS_KEY_ID,
+  secretAccessKey: process.env.NEXT_PUBLIC_SECRET_ACCESS_KEY,
   region: REGION
 });
